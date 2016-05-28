5a522a3635635ac7c29504e4c971df0abcfde61e
aneesdhansey <aneesdhansey@gmail.com>
2016-05-28 23:54:40 +0530

    Initial Commit

---
+++ client/app.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'client/app.html'
+})
+class BubbleWrapERP { }
+
+bootstrap(BubbleWrapERP);
+
